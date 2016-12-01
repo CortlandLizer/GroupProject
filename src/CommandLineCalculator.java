f240ba7122ccf6d1a0fa8dcf07f8bc86499da334
C <cortland.lizer.15@cnu.edu>
2016-12-01 16:15:35 -0500

    half done

--- src/CommandLineCalculator.java
+++ src/CommandLineCalculator.java
@@ -1,4 +1,80 @@
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CommandLineCalculator.
+ */
 public class CommandLineCalculator {
 
+    /**
+     * Store variable.
+     *
+     * @param str the str
+     */
+    private void storeVariable (String str){
+      //  - look in string for  “=” to separate variables from equations if there is one “=”store 
+      //    the variables and the equation
+            
+       //     -Save the variable as a String in a binary search tree
+
+
+      //      -Save the corresponding String equation in a hash table 
+        }
+        
+        /**
+         * Subtraction.
+         *
+         * @param x the x
+         * @param y the y
+         * @return the double
+         */
+        private double subtraction(String x, String y){
+       // - convert strings to doubles and perform the operation for subtraction
+
+            return  0.0 ;
+        // - return the double 
+        }
+        
+        /**
+         * Addition.
+         *
+         * @param x the x
+         * @param y the y
+         * @return the double
+         */
+        private double addition(String x, String y){
+       // - convert strings to doubles and perform the operation for addition
+
+            return  0.0 ;
+       // - return the double 
+        }
+        
+        /**
+         * Division.
+         *
+         * @param x the x
+         * @param y the y
+         * @return the double
+         */
+        private double division(String x, String y){
+      //  - convert strings to doubles and perform the operation for division
+
+            return  0.0 ;
+       // - return the double 
+        }
+        
+        /**
+         * Multiplication.
+         *
+         * @param x the x
+         * @param y the y
+         * @return the double
+         */
+        private double multiplication(String x, String y){
+       //- convert strings to doubles and perform the operation for multiplication
+
+
+         return  0.0 ;
+        }
+    
+    
 }
