907be5a5b89dc13770dde7e3ee7e9e1ff7108bf7
C <cortland.lizer.15@cnu.edu>
2016-12-03 15:24:26 -0500

    sadie

--- src/CommandLineCalculator.java
+++ src/CommandLineCalculator.java
@@ -1,19 +1,25 @@
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class CommandLineCalculator.
+ * The Class CommandLineCalculator is an arithmetic based expression evaluator
+ * that supports: declaring variables, addition, subtraction, multiplication,
+ * division, assigning values to variables, and assigning arithmetic expressions
+ * to variables.
+ * 
+ * @author Sadie Rynestad, Troy Potvin, and Cortland Lizer
+ * @version 12/5/2017
  */
 public class CommandLineCalculator {
     private Double answer;
     private String calculatedAnswer;
 
     /**
-     * this method checks that if the string submitted is a valid option, it
-     * checks if it is a variable that needs to be stored if it is it stores it
-     * and returns "variable stored" and if neither of those conditions are hit
-     * it uses all the other methods in this program to perform the required
-     * operations in the order of Parentheses, Multiplication, Division,
-     * Addition, Subtraction (PMDAS) and returns the final answer
+     * This method checks that if the String submitted is a valid option, then
+     * it checks if it is a variable that needs to be stored. If it is, then it
+     * stores it and returns "variable stored" and if neither of those
+     * conditions are hit it uses all the other methods in this program to
+     * perform the required operations in the order of Parentheses,
+     * Multiplication, Division, Addition, Subtraction (PMDAS) and returns the
+     * final answer
      * 
      * @param str
      *            the string inputed by the user
@@ -23,44 +29,42 @@ public class CommandLineCalculator {
      */
     public String calculator(String str) {
 
-        /*
-         * - call the private method storeVariable return “variable stored”
-         * 
-         * -replace all variable with numbers using call getVariable - look in
-         * string for the operators and do the operations in pairs calling the
-         * methods in order, Parentheses Multiplication Division Addition
-         * Subtraction (PMDAS) example:
-         */
+        // calls the private method storeVariable return “variable stored”
+
+        // replace all variable with numbers using call getVariable - look in
+        // string for the operators and do the operations in pairs calling the
+        // methods in order, Parentheses Multiplication Division Addition
+        // Subtraction (PMDAS) example:
+
         return calculatedAnswer;
     }
 
     /**
-     * this method checks if the inputed method is a string is meant to be
+     * This method checks if the inputed method is a String and is meant to be
      * stored as a variable which is determined by the use of a single "="
-     * symbol if it is meant to be a variable the it stores the solved equation
-     * and corresponding variable in a hash table.
+     * symbol. If it is meant to be a variable, then it stores the solved
+     * equation and corresponding variable in a hash table.
      * 
      * @param str
      *            the string inputed by the user
      */
     private void storeVariable(String str) {
-        // - look in string for “=” to separate variables from equations if
-        // there is one “=”store
-        // the variables and the equation
+        // -look in string for “=” to separate variables from the equations. If
+        // there is only one “=”, store the variables and the solved equation
 
-        // -Save the variable and the answer to the corresponding equation in a
+        // -save the variable and the answer to the corresponding equation in a
         // hash table
     }
 
     /**
-     * this method takes the given string and returns the answer to that given
-     * variable, if the string is not a stored variable it returns "variable not
-     * found"
+     * This method takes the given string and returns the answer to that given
+     * variable. If the string is not a stored variable, then it returns
+     * "variable not found"
      * 
      * @param str
      *            the string to be checked if it is a variable and then found in
      *            the hash table
-     * @return getVar the variables corresponding answer that was in the has
+     * @return getVar the variables corresponding answer that was in the hash
      *         table
      */
 
@@ -71,7 +75,7 @@ public class CommandLineCalculator {
     }
 
     /**
-     * this method subtracts the second variable given from the first variable
+     * This method subtracts the second variable given from the first variable
      *
      * @param x
      *            the number that you will subtract from
@@ -88,7 +92,7 @@ public class CommandLineCalculator {
     }
 
     /**
-     * this method adds the first number to the second number.
+     * This method adds the first number to the second number.
      *
      * @param x
      *            the first number to be added
@@ -104,7 +108,7 @@ public class CommandLineCalculator {
     }
 
     /**
-     * this method divides the first number by the second number.
+     * This method divides the first number by the second number.
      *
      * @param x
      *            the number that is being divided
@@ -120,7 +124,7 @@ public class CommandLineCalculator {
     }
 
     /**
-     * this method multiplies the first number by the second number
+     * This method multiplies the first number by the second number
      *
      * @param x
      *            the first number to be multiplied
@@ -136,12 +140,13 @@ public class CommandLineCalculator {
     }
 
     /**
-     * takes in the given subStrings that is between the two parentheses and
-     * sends it back through method and converts that answer to a string
+     * This method takes in the given subStrings that is between the two
+     * parentheses and sends it back through method and converts that answer to
+     * a String.
      * 
      * @param str
      *            the string inside the parentheses
-     * @return strPar value of the substring converted back to a string
+     * @return strPar value of the substring converted back to a String
      */
     private String parentheses(String str) {
         String strPar = "";
