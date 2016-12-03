8264015857273478827f03ed545b28635779a805
C <cortland.lizer.15@cnu.edu>
2016-12-03 15:49:01 -0500

    done again

--- src/CommandLineCalculatorTest.java
+++ src/CommandLineCalculatorTest.java
@@ -1,12 +1,146 @@
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
-public class CommandLineCalculatorTest {
 
+/**
+ * 
+ * @author Sadie Rynestad, Troy Potvin, and Cortland Lizer
+ * @version 12/5/2019
+ */
+
+
+public class CommandLineCalculatorTest {
+    /**
+     * The is the basic test for Calculator
+     */
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testCalculator() {
+        // test if calculator is given a variable separated by only one "=" and
+        // stores it
+
+
+        // test if calculator is given a String with more than one "=" if it
+        // should return "invalid entry"
+
+
+        // test when calculator is given a simple 2 number equation one each for
+        // Parentheses, Multiplication, Division, Addition, and Subtraction
     }
 
+
+    /**
+     * The is the complex test for Calculator
+     */
+    @Test
+    public void testCalculatorWithComplexEquation() {
+        // test if calculator is given a variable separated by only one "=" and
+        // stores it
+
+
+        // test if calculator is given a String with more than one "=" if it
+        // should return "invalid entry"
+
+
+        // test when calculator is given a complex more than 2 numbers equation
+        // one each for
+        // Parentheses, Multiplication, Division, Addition, and Subtraction
+    }
+
+
+    /**
+     * The is the complex test for Calculator with variables
+     */
+    @Test
+    public void testCalculatorWithComplexEquationAndVariables() {
+        // test if calculator is given a variable separated by only one "=" and
+        // stores it
+
+
+        // test if calculator is given a String with more than one "=" if it
+        // should return "invalid entry"
+
+
+        // test when calculator is given a complex more than 2 numbers equation
+        // with added variables, one each for
+        // Parentheses, Multiplication, Division, Addition, and Subtraction
+    }
+
+
+    /*
+     * This is the test for adding two numbers
+     */
+    @Test
+    public void testAddition() {
+
+
+        // tests by adding two numbers and testing if the answer is valid
+    }
+
+
+    /*
+     * This is the test for subtracting two numbers
+     */
+    @Test
+    public void testSubtraction() {
+
+
+        // tests by subtracting two numbers and testing if the answer is valid
+    }
+
+
+    /*
+     * This is the test for multiplying two numbers
+     */
+    @Test
+    public void testMultiplication() {
+
+
+        // tests by multiplying two numbers and testing if the answer is valid
+    }
+
+
+    /*
+     * This is the test for dividing two numbers
+     */
+    @Test
+    public void testDivision() {
+
+
+        // tests by division two numbers and testing if the answer is valid
+    }
+
+
+    /**
+     * This is the test to see if the parentheses are removed
+     */
+    @Test
+    public void testRemoveParentheses() {
+
+
+        // tests that it removes the parentheses
+
+
+    }
+
+
+    /**
+     * This is the test to check if the String was sent to the method calculator
+     */
+    @Test
+    public void testParentheses() {
+
+
+        // tests that it sends the string to the method calculator and that it
+        // receives it and returns a string
+
+
+    }
+
+
 }
+
+
+
+
