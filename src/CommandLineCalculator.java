ba15998cb39bd4b93ed671bdd7b6d866fe35b802
C <cortland.lizer.15@cnu.edu>
2016-12-03 13:22:58 -0500

    mostly done

--- src/CommandLineCalculator.java
+++ src/CommandLineCalculator.java
@@ -4,77 +4,90 @@
  * The Class CommandLineCalculator.
  */
 public class CommandLineCalculator {
+    private Double answer;
 
     /**
-     * Store variable.
-     *
-     * @param str the str
+     * this method checks if the inputed method is a string is meant to be
+     * stored as a variable which is determined by the use of a single "="
+     * symbol if it is meant to be a variable the it stores the solved equation
+     * and corresponding variable in a hash table.
+     * 
+     * @param str
+     *            the string inputed by the user
      */
-    private void storeVariable (String str){
-      //  - look in string for  “=” to separate variables from equations if there is one “=”store 
-      //    the variables and the equation
-            
-       //     -Save the variable as a String in a binary search tree
+    private void storeVariable(String str) {
+        // - look in string for “=” to separate variables from equations if
+        // there is one “=”store
+        // the variables and the equation
 
+        // -Save the variable as a String in a binary search tree
 
-      //      -Save the corresponding String equation in a hash table 
-        }
-        
-        /**
-         * Subtraction.
-         *
-         * @param x the x
-         * @param y the y
-         * @return the double
-         */
-        private double subtraction(String x, String y){
-       // - convert strings to doubles and perform the operation for subtraction
+        // -Save the corresponding String equation in a hash table
+    }
 
-            return  0.0 ;
-        // - return the double 
-        }
-        
-        /**
-         * Addition.
-         *
-         * @param x the x
-         * @param y the y
-         * @return the double
-         */
-        private double addition(String x, String y){
-       // - convert strings to doubles and perform the operation for addition
+    /**
+     * this method subtracts the second variable given from the first variable
+     *
+     * @param x
+     *            the number that you will subtract from
+     * @param y
+     *            the number that you will subtract from x
+     * @return answer the value of (x-y)
+     */
+    private double subtraction(String x, String y) {
+        // - convert strings to doubles and perform the operation for
+        // subtraction
 
-            return  0.0 ;
-       // - return the double 
-        }
-        
-        /**
-         * Division.
-         *
-         * @param x the x
-         * @param y the y
-         * @return the double
-         */
-        private double division(String x, String y){
-      //  - convert strings to doubles and perform the operation for division
+        return answer;
+        // - return the double
+    }
 
-            return  0.0 ;
-       // - return the double 
-        }
-        
-        /**
-         * Multiplication.
-         *
-         * @param x the x
-         * @param y the y
-         * @return the double
-         */
-        private double multiplication(String x, String y){
-       //- convert strings to doubles and perform the operation for multiplication
+    /**
+     * this method adds the first number to the second number.
+     *
+     * @param x
+     *            the first number to be added
+     * @param y
+     *            the second number to be added
+     * @return answer of the first number added to the second number (x+y)
+     */
+    private double addition(String x, String y) {
+        // - convert strings to doubles and perform the operation for addition
 
+        return answer;
+        // - return the double
+    }
 
-         return  0.0 ;
-        }
-    
-    
+    /**
+     * this method divides the first number by the second number.
+     *
+     * @param x
+     *            the number that is being divided
+     * @param y
+     *            the number that you are dividing by
+     * @return answer the first number divided by the second number (x/y)
+     */
+    private double division(String x, String y) {
+        // - convert strings to doubles and perform the operation for division
+
+        return answer;
+        // - return the double
+    }
+
+    /**
+     * this method multiplies the first number by the second number
+     *
+     * @param x
+     *            the first number to be multiplied
+     * @param y
+     *            the second number to be multiplied
+     * @return answer the first number multiplied by the second number (x/y)
+     */
+    private double multiplication(String x, String y) {
+        // - convert strings to doubles and perform the operation for
+        // multiplication
+
+        return answer;
+    }
+
 }
